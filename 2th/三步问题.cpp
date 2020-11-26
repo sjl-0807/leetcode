41d818d408e619680d1ada0d705de81d9d7e66bb
black <71620882+sjl-0807@users.noreply.github.com>
2020-11-26 10:56:14 +0000

    面试题08.01

---
+++ 2th/三步问题.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int waysToStep(int n) {
+        int *dp;
+        if(n<=2){
+            return n;
+        }
+        dp=new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=4;
+        for(int i=4;i<=n;i++){
+            dp[i]=(dp[i-1]+(dp[i-2]+dp[i-3])%1000000007)%1000000007;
+        }
+        return dp[n];
+    }
+};
